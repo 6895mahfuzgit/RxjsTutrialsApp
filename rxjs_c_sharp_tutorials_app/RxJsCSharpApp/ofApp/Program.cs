14606622941d0c0d333065938c3b70808ad9ecc4
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-09-21 00:54:20 +0600

    convert list to obserable list

---
+++ rxjs_c_sharp_tutorials_app/RxJsCSharpApp/ofApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace ToObservableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IObservable<int> numbers = Observable.ToObservable<int>(new List<int> { 1, 2, 3, 4, 5, 6 });
+            numbers.Take(3)
+                   .Aggregate((a, b) => a + b)
+                   .Subscribe(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
