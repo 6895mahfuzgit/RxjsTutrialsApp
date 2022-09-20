c9dab7cce0bf3b2e29df7410c9361e7e745112fe
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-09-21 00:44:11 +0600

    Interval

---
+++ rxjs_c_sharp_tutorials_app/RxJsCSharpApp/IntervalApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace IntervalApp
+{
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Counter().timer.Subscribe(data =>
+            {
+                Console.WriteLine(data);
+            });
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Counter
+    {
+        public IObservable<long> timer = Observable.Interval(TimeSpan.FromSeconds(1)).TakeUntil(counter => counter == 5);
+    }
+}
