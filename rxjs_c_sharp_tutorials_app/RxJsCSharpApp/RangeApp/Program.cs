aa6ad340cb1517299e9c484505941bf785e3c6cc
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-09-21 01:02:01 +0600

    range

---
+++ rxjs_c_sharp_tutorials_app/RxJsCSharpApp/RangeApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RangeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IObservable<int> range = Observable.Range(1, 100).TakeLast(10);
+
+            range.Subscribe(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
