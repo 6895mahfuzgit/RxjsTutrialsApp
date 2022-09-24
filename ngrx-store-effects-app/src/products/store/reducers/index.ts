b7877e190abbb009a69b1721ef1672912a94d9f7
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-09-24 14:21:10 +0600

    Update index.ts

--- ngrx-store-effects-app/src/products/store/reducers/index.ts
+++ ngrx-store-effects-app/src/products/store/reducers/index.ts
@@ -1,4 +1,6 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createSelector, createFeatureSelector } from '@ngrx/store';
+import { stat } from 'fs';
+import { StoreEnum } from '../enum/store-enum';
 import * as fromPizzas from './pizzas.reducer'
 
 export interface ProductsState {
@@ -8,3 +10,22 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
     pizzas: fromPizzas.reducer
 };
+
+export const getProductsState = createFeatureSelector<ProductsState>(StoreEnum.PRODUCTS);
+
+//pizzas state 
+export const getPizzaState = createSelector(
+    getProductsState,
+    (state: ProductsState) => state.pizzas);
+
+export const getAllPizzas = createSelector(
+    getPizzaState,
+    fromPizzas.getPizzas);
+
+export const getPizzasLoading = createSelector(
+    getPizzaState,
+    fromPizzas.getPizzasLoading);
+
+export const getPizzasLoaded = createSelector(
+    getPizzaState,
+    fromPizzas.getPizzasLoaded);
