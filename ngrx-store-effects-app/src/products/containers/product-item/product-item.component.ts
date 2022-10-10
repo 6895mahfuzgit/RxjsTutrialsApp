3d6da83b89d350c1eb72698c2cdc056feca495af
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-10-10 07:20:48 +0600

    Update product-item.component.ts

--- ngrx-store-effects-app/src/products/containers/product-item/product-item.component.ts
+++ ngrx-store-effects-app/src/products/containers/product-item/product-item.component.ts
@@ -16,7 +16,7 @@ import { Observable } from 'rxjs';
       class="product-item">
       <pizza-form
         [pizza]="pizza$ | async"
-        [toppings]="toppings"
+        [toppings]="toppings$ | async"
         (selected)="onSelect($event)"
         (create)="onCreate($event)"
         (update)="onUpdate($event)"
@@ -31,12 +31,13 @@ import { Observable } from 'rxjs';
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
   visualise: Pizza;
-  toppings: Topping[];
+  toppings$: Observable<Topping[]>;
 
   constructor(private store:Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
     this.store.dispatch(new fromStore.LoadToppings())
+    this.toppings$=this.store.select(fromStore.getAllToppings)
     this.pizza$=this.store.select(fromStore.getSelectedPizza);
   }
 
