b4e857336f64943c6cb35890eec45a3445f9b5db
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-10-10 10:37:42 +0600

    Update toopings.action.ts

--- ngrx-store-effects-app/src/products/store/actions/toopings.action.ts
+++ ngrx-store-effects-app/src/products/store/actions/toopings.action.ts
@@ -2,10 +2,12 @@ import { type } from 'os';
 import { Topping } from './../../models/topping.model';
 import { Action } from '@ngrx/store';
 
-
+//toppings
 export const LOAD_TOPPINGS = '[Products] Load Toppings';
 export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
 export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
+
+export const VISUALISE_TOPPINGS = '[Products] Visualise Toppings';
 
 export class LoadToppings implements Action {
     readonly type = LOAD_TOPPINGS;
@@ -21,6 +23,10 @@ export class LoadToppingsSuccess implements Action {
     constructor(public payload: Topping[]) { }
 }
 
+export class VisualiseToppings implements Action {
+    readonly type = VISUALISE_TOPPINGS;
+    constructor(public payload: number[]) { }
+}
 
-export type ToppingsAction = LoadToppings | LoaToppingsFail | LoadToppingsSuccess;
+export type ToppingsAction = LoadToppings | LoaToppingsFail | LoadToppingsSuccess | VisualiseToppings;
 
